3abd8ed8c9d8ef6d286bc339524bb6f12b19869e
LAK132 <lakridesagain@gmail.com>
2019-09-15 12:02:10 +0930

    Add rolling average for temperature readings

--- ATtiny-throttle-control.ino
+++ ATtiny-throttle-control.ino
@@ -6,13 +6,13 @@
 // Pin 4 - PWM, Analog (also used for USB- when USB is in use)
 // Pin 5 - Analog In
 
-const int TempSensorPin = 0;   // Pin 5 (Analog)
-const int ControlValvePin = 1; // Pin 1
+#include <limits.h>
 
-const unsigned long MaxOnTime = 10000; // Time in millis
-const int CutOffTemp = 100; // Min analog value
-
-bool valve_on = true;
+const int TempSensorPin = 0;            // Pin 5 (Analog)
+const int ControlValvePin = 1;          // Pin 1
+const unsigned long MaxOnTime = 10000;  // Time in millis
+const int CutOffTemp = 100;             // Min analog value of temperature sensor
+const int MaxReadings = 100;            // Number of temperature values to average
 
 void setup()
 {
@@ -20,12 +20,24 @@ void setup()
   digitalWrite(ControlValvePin, HIGH);
 }
 
+bool valve_on = true;
+int readings[MaxReadings] = {INT_MAX};
+int reading_index = 0;
+
 void loop()
 {
   if (valve_on)
   {
-    if (millis() > MaxOnTime ||
-        analogRead(TempSensorPin) < CutOffTemp)
+    // Rolling average of temperature readings
+    if (reading_index >= MaxReadings) reading_index = 0;
+    readings[reading_index++] = analogRead(TempSensorPin);
+
+    long readings_average = 0;
+    for (int i = 0; i < MaxReadings; ++i)
+      readings_average += readings[i];
+    readings_average /= MaxReadings;
+
+    if (millis() > MaxOnTime || readings_average < CutOffTemp)
     {
       valve_on = false;
       digitalWrite(ControlValvePin, LOW);
